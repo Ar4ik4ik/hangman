3d5222da3b423a92d17e405af21f8ff04a5553b2
Arturo <arthursolokhin27@gmail.com>
2024-11-28 20:39:31 +0300

    finished game

--- gameCycle.py
+++ gameCycle.py
@@ -11,28 +11,39 @@ def get_random_word():
         return word[0]
 
 
-def set_game(word: str):
-    hidden_word = '_' * len(word)
+def set_game():
+    word = get_random_word()
+    hidden_word = ['_'] * len(word)
     mistakes_count = 0
     hangman_img = print_hangman(0)
+    exists_chrs = []
     while mistakes_count != 6:
-        print(hidden_word)
-        input_chr = input("Please enter the char: ")
+        print(' '.join(hidden_word))
+        input_chr = input("Please enter the char: ").lower()
+        if input_chr in exists_chrs:
+            continue
+        exists_chrs.append(input_chr)
         if input_chr in word:
-            for char, i in enumerate(word):
-                if input_chr == char:
-                    converted_word = list(word)
-                    converted_word[i] = char
-                    hidden_word = str(converted_word)
-        # else:
+            for i, char in enumerate(word):
+                if char == input_chr:
+                    hidden_word[i] = input_chr
+        else:
+            mistakes_count += 1
+            print_hangman(mistakes_count)
+            print('\n')
+            print('\n')
+            print(f"Количество ошибок: {mistakes_count}")
+
+        if ''.join(hidden_word) == word:
+            print("Ты выиграл !")
+            exit_or_start()
+
+    print(f"Ты проиграл !\n"
+          f"{' '.join(hidden_word)}")
+    exit_or_start()
+    return
 
 
-# ._____
-# |    |
-# |    o
-# |   /|\
-# |   / \
-# |
 def print_hangman(mistakes: int) -> str:
     start_img = ("                  ._____\n"
                  "                  |    |\n"
@@ -63,4 +74,8 @@ def print_hangman(mistakes: int) -> str:
     print('\n'.join(stripped_img))
 
 
-print_hangman(1)
+def exit_or_start():
+    if input('Exit or start new game ?: ') == 'start':
+        set_game()
+
+exit_or_start()
