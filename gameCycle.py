0eae748c5dedea808af4da295f31a98d3ae7de68
Arturo <arthursolokhin27@gmail.com>
2024-11-28 20:52:21 +0300

    slice fn + input validation

--- gameCycle.py
+++ gameCycle.py
@@ -11,22 +11,29 @@ def get_random_word():
         return word[0]
 
 
+def show_char(input_word, hidden_word, entered_char):
+    for i, char in enumerate(input_word):
+        if char == entered_char:
+            hidden_word[i] = entered_char
+    return hidden_word
+
+
 def set_game():
     word = get_random_word()
     hidden_word = ['_'] * len(word)
     mistakes_count = 0
-    hangman_img = print_hangman(0)
     exists_chrs = []
     while mistakes_count != 6:
         print(' '.join(hidden_word))
         input_chr = input("Please enter the char: ").lower()
+        if not input_chr.isalpha():
+            continue
         if input_chr in exists_chrs:
             continue
         exists_chrs.append(input_chr)
+
         if input_chr in word:
-            for i, char in enumerate(word):
-                if char == input_chr:
-                    hidden_word[i] = input_chr
+            show_char(word, hidden_word, input_chr)
         else:
             mistakes_count += 1
             print_hangman(mistakes_count)
@@ -77,5 +84,6 @@ def print_hangman(mistakes: int) -> str:
 def exit_or_start():
     if input('Exit or start new game ?: ') == 'start':
         set_game()
+
 
 exit_or_start()
